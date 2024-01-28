62ed1f79790e8f14fb912371233eba5b8f5da6ca
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-28 17:14:58 +0100

    Styling och js

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,26 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  DocumentReference,
+} from "firebase/firestore";
 
+export {
+  app,
+  data,
+  dbRef,
+  addDoc,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  DocumentReference,
+};
 const firebaseConfig: any = {
   apiKey: "AIzaSyBwikN3hWkM1zkAK9T_e7uAYG2Ql67U3ZM",
   authDomain: "todo-list-a0a13.firebaseapp.com",
@@ -10,30 +30,8 @@ const firebaseConfig: any = {
   appId: "1:219957146252:web:87fc9408c86649760c81a9",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 console.log(app);
 
-const db = getFirestore(app);
-console.log(db);
-
-/* ADD DOC TAR TVÅ ARUGEMENT */
-/* Sätter ett redan valt ID */
-
-addDoc(collection(db, "users"), {
-  todo: "Äta pizza",
-  comment: "Beställa foodora",
-})
-  .then((docRef) => {
-    // Visar om vi lyckats lägga till data i firestore
-    console.log(docRef.id);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
-/* Setdoc för att ge sitt egna ID*/
-/* För att använda setDock behövs 
-DB- vart vi vi vi lägg till data
-DOC(), vart vi vi passa DB, collection namn och custom ID,
-SETDOCK vart vi passar datan med Doc() Metoden*/
+const data = getFirestore();
+const dbRef = collection(data, "todos");
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,176 @@
-let array: number[] = [1, 2, 3, 4];
+import {
+  app,
+  data,
+  dbRef,
+  addDoc,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  DocumentReference,
+} from "./firebase";
+console.log(app, data, dbRef);
+
+const addTodoBtn = document.getElementById(
+  "add-todo-form"
+) as HTMLButtonElement | null;
+const todoInput = document.getElementById(
+  "todo-input"
+) as HTMLInputElement | null;
+
+if (addTodoBtn && todoInput) {
+  addTodoBtn.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const todoValue: string | undefined = getTodo();
+    console.log(todoValue);
+
+    const todoId = todoInput.getAttribute("todo-id");
+
+    if (todoId !== null) {
+      const docRef = doc(data, "todos", todoId);
+
+      try {
+        await updateDoc(docRef, {
+          todo: todoValue,
+        });
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        await addDoc(dbRef, {
+          todo: todoValue,
+        });
+        todoInput.value = "";
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  });
+}
+
+const getTodo = (): string | undefined => {
+  if (!todoInput) {
+    return undefined;
+  }
+
+  const trimmedValue = todoInput.value.trim();
+  if (trimmedValue === "") {
+    return undefined;
+  }
+
+  return trimmedValue;
+};
+
+// ----------------------
+// Hämtar datan
+// ----------------------
+
+interface Todo {
+  id: string;
+  todo: string;
+}
+
+let todos: Todo[] = [];
+
+const getTodos = async () => {
+  try {
+    await onSnapshot(dbRef, (docsSnap) => {
+      todos = [];
+      docsSnap.forEach((doc) => {
+        const todo = doc.data();
+        todo.id = doc.id;
+        todos.push({ id: doc.id, todo: todo.todo });
+      });
+      showTodos(todos);
+    });
+  } catch (err) {
+    console.log("Hämtar to do =" + err);
+  }
+};
+
+getTodos();
+
+// ----------------------
+// Visar datan
+// ----------------------
+const showTodos = (todos: Todo[]) => {
+  const displayTodos = document.getElementById(
+    "todo-list"
+  ) as HTMLElement | null;
+
+  if (displayTodos) {
+    displayTodos.innerHTML = "";
+
+    todos.forEach((item) => {
+      const li = `<li class="todo-item" id="${item.id}">
+        <div class="todo-text"><p>${item.todo}</p></div>
+        <div class="todo-btns">
+          <button class="edit">Edit</button> <button class="delete">Delete</button>
+        </div>
+      </li>`;
+
+      displayTodos.innerHTML += li;
+    });
+  }
+};
+
+// ----------------------
+// Finding Edit & Delete btns
+// ----------------------
+
+const todoListPressed = (event: Event): void => {
+  const id: string | null =
+    (event.target as HTMLElement).closest("li")?.getAttribute("id") || null;
+
+  if ((event.target as HTMLElement).classList.contains("edit")) {
+    editButtonPressed(id);
+    console.log("edit knappen", id);
+  } else if ((event.target as HTMLElement).classList.contains("delete")) {
+    deleteButtonPressed(id);
+    console.log("fungerar", id);
+  } else {
+    return undefined;
+  }
+};
+
+const listedTodo = document.getElementById("todo-list") as HTMLElement | null;
+listedTodo?.addEventListener("click", todoListPressed);
+
+// ----------------------
+// Edit To do
+// ----------------------
+const editButtonPressed = (id: string | null): void => {
+  if (!id) {
+    console.error("Invalid todo ID");
+    return;
+  }
+
+  const todoToEdit = todos.find((todo) => todo.id === id);
+
+  if (todoToEdit) {
+    todoInput!.value = todoToEdit.todo;
+    todoInput!.setAttribute("todo-id", todoToEdit.id);
+  } else {
+    console.error("Todo not found");
+  }
+};
+
+// ----------------------
+// Delete To do
+// ----------------------
+const deleteButtonPressed = async (id: string | null): Promise<void> => {
+  if (!id) {
+    console.error("Invalid todo ID");
+    return;
+  }
+
+  const docRef: DocumentReference = doc(data, "todos", id);
+
+  try {
+    await deleteDoc(docRef);
+    console.log("Todo deleted successfully");
+  } catch (error) {
+    console.error("Failed to delete todo:", error);
+  }
+};
