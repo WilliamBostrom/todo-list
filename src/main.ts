c10b4c38866dc97ba7b455f188f65f6479f24aef
WilliamBostrom <133894488+WilliamBostrom@users.noreply.github.com>
2024-01-29 18:05:05 +0100

    Börja bygga Auth

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
-import "./style.css";
-// import "./script.ts";
-// import "./firebase.ts";
+import "./css/style.css";
+import "./css/login.css";
+import "./script.ts";
+import "./firebase.ts";
--- src/script.ts
+++ src/script.ts
@@ -33,6 +33,8 @@ if (addTodoBtn && todoInput) {
         await updateDoc(docRef, {
           todo: todoValue,
         });
+        todoInput.value = "";
+        todoInput.removeAttribute("todo-id");
       } catch (e) {
         console.log(e);
       }
@@ -103,12 +105,31 @@ const showTodos = (todos: Todo[]) => {
     displayTodos.innerHTML = "";
 
     todos.forEach((item) => {
-      const li = `<li class="todo-item" id="${item.id}">
-        <div class="todo-text"><p>${item.todo}</p></div>
-        <div class="todo-btns">
-          <button class="edit">Edit</button> <button class="delete">Delete</button>
+      const li = ` <li class="todo-item" id="${item.id}">
+      <button class="todo-check" data-todo-id="${item.id}">
+        <img class="circle-img" src="./src/img/circle.svg" alt="circle"
+       />
+      </button>
+      <div class="todo-text" data-p-id="${item.id}"><p>${item.todo}</p></div>
+      <div class="todo-btns">
+        <div class="todo-edit-del">
+          <button class="delete">
+            <img
+              class="delete-img"
+              src="./src/img/trash-svgrepo-com.svg"
+              alt="delete button"
+            />
+          </button>
+          <button class="edit">
+            <img
+              class="edit-img"
+              src="./src/img/edit-3-svgrepo-com.svg"
+              alt="edit button"
+            />
+          </button>
         </div>
-      </li>`;
+      </div>
+    </li>`;
 
       displayTodos.innerHTML += li;
     });
@@ -123,19 +144,22 @@ const todoListPressed = (event: Event): void => {
   const id: string | null =
     (event.target as HTMLElement).closest("li")?.getAttribute("id") || null;
 
-  if ((event.target as HTMLElement).classList.contains("edit")) {
+  if ((event.target as HTMLElement).classList.contains("edit-img")) {
     editButtonPressed(id);
     console.log("edit knappen", id);
-  } else if ((event.target as HTMLElement).classList.contains("delete")) {
+  } else if ((event.target as HTMLElement).classList.contains("delete-img")) {
     deleteButtonPressed(id);
     console.log("fungerar", id);
+  } else if ((event.target as HTMLElement).classList.contains("circle-img")) {
+    circleButtonPressed(id);
+    console.log("circle", id);
   } else {
     return undefined;
   }
 };
 
 const listedTodo = document.getElementById("todo-list") as HTMLElement | null;
-listedTodo?.addEventListener("click", todoListPressed);
+listedTodo?.addEventListener("mousedown", todoListPressed);
 
 // ----------------------
 // Edit To do
@@ -172,5 +196,39 @@ const deleteButtonPressed = async (id: string | null): Promise<void> => {
     console.log("Todo deleted successfully");
   } catch (error) {
     console.error("Failed to delete todo:", error);
+  }
+};
+
+// ----------------------
+// Checkbox To do
+// ----------------------
+let checked: boolean = false;
+const circleButtonPressed = (todoId: string | null): void => {
+  if (!todoId) {
+    console.error("Invalid todo ID");
+    return;
+  }
+
+  checked = !checked;
+
+  const circleImg: HTMLImageElement | null = document.querySelector(
+    `[data-todo-id="${todoId}"] .circle-img`
+  );
+  const todoParagraph: HTMLParagraphElement | null = document.querySelector(
+    `[data-p-id="${todoId}"] p`
+  );
+
+  if (circleImg && todoParagraph) {
+    if (!checked) {
+      circleImg.src = "./src/img/circle.svg";
+      circleImg.alt = "circle";
+      todoParagraph.style.textDecoration = "none";
+    } else {
+      circleImg.src = "./src/img/circle-checked.svg";
+      circleImg.alt = "circle checked";
+      todoParagraph.style.textDecoration = "line-through";
+    }
+  } else {
+    console.error("Circle image or paragraph element not found");
   }
 };
